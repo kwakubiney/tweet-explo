6e9b8ab1e1f9004a9972128543f68db98530e89a
kweku-45 <kebiney@hotmail.com>
2021-12-14 20:00:16 +0000

    Create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Tweet(models.Model):
+    tweet = models.CharField(max_length=280 , blank=False, default=None)
+    date_created = models.DateTimeField()
+    time_created = models.TimeField()
+    username = models.CharField(max_length=15, blank=False, default=None)
+
